491c3ec5cb0c95f09d1d4db7ae459e29c94137a7
Irina Koshman <koshman.irina@yandex.ru>
2023-02-16 19:20:11 +0300

    Добавили решение задачи

---
+++ Controlwork/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+string[] startArray = newArray();
+
+string[] newArray()
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+int lengtsSymbol = 3;
+
+string[] resultArray = new string[startArray.Length];
+int i = 0;
+for (int j = 0; j < startArray.Length; j++)
+{
+    if (startArray[j].Length <= lengtsSymbol)
+    {
+        resultArray[i] = startArray[j];
+        i++;
+    }
+}
+
+Console.WriteLine($"[{String.Join(", ", startArray)}] -> [{String.Join(", ", resultArray)}]");
+
+
